c4694f309431012a9b7b57c791a1185d271a2f79
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-08 15:20:03 +0800

    setup angular and partial rendering

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ app.use(function(req, res, next) {
 app.use(require('./middlewares/database'));
 
 app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.use('/api/*', require('./api/index'));
+
+// render partials for angular
+router.get('/partials/:filename', function (req, res, next) {
+  var filename = req.params.filename,
+      locals;
+
+  if (!filename) {
+    return next();
+  }
+
+  if (req.session.passport) {
+    locals = {user: req.session.passport.user};
+  }
+
+  filename = filename.replace('-', '/').replace('..', '');
+  res.render("partials/" + filename, locals);
+});
+
+// catch all for loading angular page
+router.all('/*', function (req, res) {
+  res.render('index', {title: 'Kardboard'});
+});
+
+
 module.exports = router;
