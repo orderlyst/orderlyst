aec4477648e6be1ee75566267446a20b0bb2fdde
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-09 15:42:21 +0800

    added update user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,13 +6,13 @@ router.get('/:id', function(req, res, next) {
   var userId = req.params.id;
   UserModel
     .findOne({
-      _id: id
+      "_id": userId
     })
     .exec(function(err, user){
       if (user) {
         res.json(user);
       } else {
-        next();
+        next(err);
       }
     });
 });
@@ -27,6 +27,25 @@ router.post('/', function(req, res, next) {
       res.json(user);
     }
   });
+});
+
+router.post('/:id', function(req, res, next) {
+  var name = req.body.name;
+  var userId = req.params.id;
+
+  UserModel
+    .findOne({
+      "_id": userid
+    })
+    .exec(function(err, user){
+      user.name = name;
+      user.save();
+      if (user) {
+        res.json(user);
+      } else {
+        next(err);
+      }
+    })
 });
 
 module.exports = router;
