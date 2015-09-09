1aae91a09f16bc99052ec93e20c0a88be6c57013
Louis Lai <hoangdung2506@gmail.com>
2015-09-09 21:31:04 +0800

    Add createOrder controller but not working yet

--- assets/scripts/controllers/orderController.js
+++ assets/scripts/controllers/orderController.js
@@ -2,18 +2,41 @@ var joinOrder = function($scope, $http, $location) {
     $scope.submit = function(joinOrder) {
         var name = joinOrder.name;
         var orderCode = joinOrder.code;
-        if (name == "" || orderCode == "") return;
-        // First create user
+        if (name === "" || orderCode === "") return;
+        console.log(name);
+        console.log(orderCode);
+        // First create user and go to order page
         $http.post(
             '/api/user',
             {name: name}
         ).success(function(data) {
            $location.url('/orders/' + orderCode);
         });
-
     };
 };
 
+var createOrder = function($scope, $http, $location) {
+    $scope.submit = function(joinOrder) {
+        var name = joinOrder.name;
+        if (name === "" || orderCode === "") return;
+        console.log(name);
+        console.log(orderCode);
+        // First create user and go to order page
+        $http.post(
+            '/api/user',
+            {name: name}
+        ).success(function(data) {
+            return $http.post(
+                    '/api/orders',
+                    {hostUserId: data._id});
+        }).success(function(data) {
+            $location.url('/orders/' + data.code);
+        });
+    };
+};
+
+
 module.exports = {
-    joinOrder: joinOrder
-}
+    joinOrder: joinOrder,
+    createOrder: createOrder
+};
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,14 +2,15 @@ require('angular');
 require('angular-animate');
 require('angular-route');
 
-var OrderController = require('./controllers/orderController.js');
+var OrderController = require('./controllers/orderController');
 
 // @ngInject
 var app = angular.module('orderlyst', [
       'ngAnimate',
       'ngRoute'
     ]).
-    controller('JoinOrderController', OrderController.joinOrder);
+    controller('JoinOrderController', OrderController.joinOrder).
+    controller('CreateOrderController', OrderController.createOrder);
 
 require('./routes/index')(app);
 
