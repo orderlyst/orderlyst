6aaa97161244427b96a3be82661e1fc564d75bdb
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-09 15:44:08 +0800

    created order subrouter

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var subrouters = [
-  "/users"
+  "/users",
+  "/orders"
 ];
 
 subrouters.forEach(function(route){
---
+++ routes/api/orders.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+var OrderModel = require('../../models/order');
+
+
+module.exports = router;
