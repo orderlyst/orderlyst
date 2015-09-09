96c6d7d58b8b5c16ed0edd71e60afd360cb9d06c
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-09 15:53:51 +0800

    added route to get order's items

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -46,4 +46,22 @@ router.post('/', function(req, res, next) {
 
 });
 
+/**
+ * To fetch an order's items
+ */
+router.get('/:id/items', function(req, res, next) {
+  var orderId = req.params.id;
+  OrderModel
+    .findOne({
+      "_id": orderId
+    })
+    .exec(function(err, order){
+      if (order) {
+        res.json(order.items);
+      } else {
+        next(err);
+      }
+    });
+});
+
 module.exports = router;
