36b4b1c0c3ae375ac8d72303b8b65ca047d49baf
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-09 14:23:04 +0800

    register subrouters

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var subrouters = [
+  "/users"
+];
 
-
+subrouters.forEach(function(router){
+  router.use(router, require('.' + router));
+});
 
 module.exports = router;
