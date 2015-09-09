5085948e95d1ea5d61f4803f6da79e8232d350e7
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-09 15:58:19 +0800

    added route to create new order

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -64,4 +64,39 @@ router.get('/:id/items', function(req, res, next) {
     });
 });
 
+/**
+ * To create a new order item
+ */
+router.post('/:id/items', function(req, res, next) {
+  var orderId = req.params.id;
+  var userId = req.body.user;
+  var join = Join.create();
+  OrderModel
+    .findOne({
+      "_id": orderId
+    })
+    .exec(join.add());
+
+  UserModel
+    .findOne({
+      "_id": userId
+    })
+    .exec(join.add());
+
+  join.then(function(orderCallback, userCallback){
+    var order = orderCallback[1];
+    var user = userCallback[1];
+    if (order && user)
+      order.items.push({
+        name: req.body.name,
+        price: req.body.price,
+        user: user._id
+      });
+      res.json({status: '200 OK'});
+    } else {
+      next();
+    }
+  }).
+});
+
 module.exports = router;
