d2bcddcbd12a77c7fb413467fec4ac58afa611ca
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-09 15:44:43 +0800

    added order fetch

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -2,5 +2,23 @@ var express = require('express');
 var router = express.Router();
 var OrderModel = require('../../models/order');
 
+/**
+ * To fetch an order's information
+ */
+router.get('/:id', function(req, res, next) {
+  var orderId = req.params.id;
+  OrderModel
+    .findOne({
+      "_id": orderId
+    })
+    .exec(function(err, order){
+      if (order) {
+        res.json(order);
+      } else {
+        next(err);
+      }
+    });
+});
+
 
 module.exports = router;
