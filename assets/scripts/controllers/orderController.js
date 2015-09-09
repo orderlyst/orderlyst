cfabf69ea625ab5780ba52cc409a58f6070b7436
Louis Lai <hoangdung2506@gmail.com>
2015-09-09 18:10:50 +0800

    fix syntax error in orderController

--- assets/scripts/controllers/orderController.js
+++ assets/scripts/controllers/orderController.js
@@ -2,11 +2,12 @@ var joinOrder = function($scope, $http, $location) {
     $scope.submit = function(joinOrder) {
         var name = joinOrder.name;
         var orderCode = joinOrder.code;
+        if (name == "" || orderCode == "") return;
         // First create user
         $http.post(
             '/api/user',
             {name: name}
-        ).success(function(data)) {
+        ).success(function(data) {
            $location.url('/orders/' + orderCode);
         });
 
