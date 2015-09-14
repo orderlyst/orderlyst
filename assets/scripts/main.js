dba9e8ade180c421b0e267aded16c15685019efb
Louis Lai <hoangdung2506@gmail.com>
2015-09-14 21:26:15 +0800

    Remove buggy require in main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -18,7 +18,6 @@ app.controller('JoinOrderController', OrderController.joinOrder).
     controller('CreateOrderController', OrderController.createOrder).
     controller('ViewOrderController', OrderController.viewOrder).
     controller('StartOrderController', OrderController.startOrder).
-    controller('NavBarController', NavBarController).
     // User authentication
     run(['$rootScope', '$store', function($rootScope, $store) {
         $store.bind($rootScope, '_orderlyst_uid', -1);
