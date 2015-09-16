478504178c8fa7b2c46f78e6b49434da6158d471
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-16 13:08:07 +0800

    removed base36 for id transform

--- middlewares/id-transform.js
+++ middlewares/id-transform.js
@@ -2,12 +2,12 @@ var Key = '431841948175129418314';
 var KeyLength = Key.length;
 
 module.exports = function(id) {
-  id = '' + parseInt(id, 36);
+  id = '' + id;
   var result = '';
   for (var i = 0, len = id.length; i < len; i++) {
     num = 0 + id[i];
     cKey = 0 + Key[i % KeyLength];
     result = '' + (num ^ cKey);
   }
-  return parseInt(result).toString(36);
+  return result;
 };
