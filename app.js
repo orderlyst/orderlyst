0d9c91c708fe87fcc9a827caf3985b46ac355f12
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-20 00:01:58 +0800

    added ws middleware

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('./middlewares/ws'));
+
 app.disable('view cache');
 app.disable('etag');
 app.use(function(req, res, next) {
