c69465fa90551d5f66de4940e1cf4f19676f76ac
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-20 09:57:07 +0800

    fixed up orderService

--- assets/scripts/services/orderService.js
+++ assets/scripts/services/orderService.js
@@ -1,59 +1,59 @@
 module.exports = function(module) {
   'use strict';
 
-  var stream = $websocket('wss://localhost:8080/wsctrl');
-
-  var orderId;
-  var order;
-  var items;
-  var userDictionary = {};
-
-  var callbacks = [];
-
-  var fetchUserDetail = function (uid) {
-    var deferred = $q.defer();
-    if (userDictionary[uid] !== undefined) {
-      deferred.reject();
-      return deferred.promise;
-    }
-    $http.get('/api/users/' + uid)
-      .success(function (data) {
-        userDictionary[uid] = data;
-        deferred.resolve(data);
-      });
-    return deferred.promise;
-  };
-
-  var fetchItems = function(orderId) {
-    var deferred = $q.defer();
-    var promises = [];
-    $http.get('/api/orders/' + orderId + '/items')
-      .success(function (data) {
-        items = data;
-        items.map(function(item) {
-          promises.push(fetchUserDetail(item.UserUserId));
-        });
-      });
-
-    $q.all(promises).then(function() {
-      deferred.resolve(items);
-    });
-    return deferred.promise;
-  };
-
-  var fetchOrder = function(orderId) {
-    var deferred = $q.defer();
-    $http.get(
-        '/api/orders/' + orderId
-      )
-      .then(function(data) {
-        order = data;
-        deferred.resolve();
-      });
-    return deferred.promise;
-  };
-
   module.factory("$order", function ($websocket, $q, $rootScope) {
+
+    var stream = $websocket('ws://localhost:8080/wsctrl');
+
+    var orderId;
+    var order;
+    var items;
+    var userDictionary = {};
+
+    var callbacks = [];
+
+    var fetchUserDetail = function (uid) {
+      var deferred = $q.defer();
+      if (userDictionary[uid] !== undefined) {
+        deferred.reject();
+        return deferred.promise;
+      }
+      $http.get('/api/users/' + uid)
+        .success(function (data) {
+          userDictionary[uid] = data;
+          deferred.resolve(data);
+        });
+      return deferred.promise;
+    };
+
+    var fetchItems = function(orderId) {
+      var deferred = $q.defer();
+      var promises = [];
+      $http.get('/api/orders/' + orderId + '/items')
+        .success(function (data) {
+          items = data;
+          items.map(function(item) {
+            promises.push(fetchUserDetail(item.UserUserId));
+          });
+        });
+
+      $q.all(promises).then(function() {
+        deferred.resolve(items);
+      });
+      return deferred.promise;
+    };
+
+    var fetchOrder = function(orderId) {
+      var deferred = $q.defer();
+      $http.get(
+          '/api/orders/' + orderId
+        )
+        .then(function(data) {
+          order = data;
+          deferred.resolve();
+        });
+      return deferred.promise;
+    };
 
     stream.onMessage(function(message) {
       if (message === 'update') {
@@ -72,7 +72,9 @@ module.exports = function(module) {
 
     var methods = {
       "register": function(oid) {
+        console.log('Registering Order ' + oid);
         orderId = oid;
+        stream.send(orderId);
       },
       "bind": function(callback) {
         if (callback) {
