9ed8a5a9a98e38a82c032b1284bb9a798d1fcb91
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-23 11:54:56 +0800

    added route to close fb share

--- routes/orders/index.js
+++ routes/orders/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/fbshareclose', function(req, res, next) {
+  res.send('<html><script>window.close()</script></html>');
+});
+
 router.get('/join/:code', function(req, res, next){
   var code = req.params.code;
   var r = require('ua-parser').parse(req.headers['user-agent']);
