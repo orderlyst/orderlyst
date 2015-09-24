2e599fb1fc1dd93f07947ca90231d4621047273d
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-24 16:34:00 +0800

    removed Facebook bot check

--- routes/orders/index.js
+++ routes/orders/index.js
@@ -7,31 +7,25 @@ router.get('/fbshareclose', function(req, res, next) {
 
 router.get('/join/:code', function(req, res, next){
   var code = req.params.code;
-  var r = require('ua-parser').parse(req.headers['user-agent']);
 
-  var isFacebookBot = (r.ua.family === 'FacebookBot');
-  if (isFacebookBot) {
-    req.models.Order
-      .find({
-        "where": {
-          "code": code,
-          "isOpen": true
-        }
-      })
-      .then(function(order){
-        if (order) {
-          console.log('loading meta');
-          // render page with order
-          res.render('meta', {
-            order: order
-          });
-        } else {
-          next();
-        }
-      });
-  } else {
-    next();
-  }
+  req.models.Order
+    .find({
+      "where": {
+        "code": code,
+        "isOpen": true
+      }
+    })
+    .then(function(order){
+      if (order) {
+        console.log('loading meta');
+        // render page with order
+        res.render('meta', {
+          order: order
+        });
+      } else {
+        next();
+      }
+    });
 });
 
 module.exports = router;
