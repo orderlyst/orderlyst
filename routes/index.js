592947803429a978e44691e1afb8fb2c25067700
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-24 22:31:34 +0800

    moved share code to index

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,41 @@ router.get('/partials/:filename', function (req, res, next) {
   res.render("partials/" + filename, {});
 });
 
+
+router.get('/fbshareclose', function(req, res, next) {
+  res.send('<html><script>window.close()</script></html>');
+});
+
+router.get('/join/:code', function(req, res, next){
+  var code = req.params.code;
+  var _order;
+
+  req.models.Order
+    .find({
+      "where": {
+        "code": code,
+        "isOpen": true
+      }
+    })
+    .then(function(order){
+      if (order) {
+        _order = order;
+        console.log('loading meta');
+        // render page with order
+        return verifier.generate();
+      } else {
+        next();
+      }
+    })
+    .then(function(token){
+      res.render('meta', {
+        order: _order,
+        token: token
+      });
+    });
+});
+
+
 // catch all for loading angular page
 router.all('/*', function (req, res) {
   verifier.generate().then(function(token){
