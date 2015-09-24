366990ce2237937e61f94d1ca24842b998539cfa
Sam-Mauris Yong <mauris@hotmail.sg>
2015-09-24 09:39:52 +0800

    added rate limiter for api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var RateLimit = require('express-rate-limit');
 var router = express.Router();
 
 var subrouters = [
   "/users",
   "/orders"
 ];
+
+// default options shown below
+var limiter = RateLimit({
+  windowMs: 120 * 1000,
+  delayAfter: 10,
+  delayMs: 500,
+  max: 20
+});
+
+// for an API-only web app, you can apply this globally
+router.use(limiter);
 
 subrouters.forEach(function(route){
   router.use(route, require('.' + route));
